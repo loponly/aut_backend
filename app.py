58c121db423aca4296c3cc23a4e31e77a5951c44
enkhbat <enkhbatlop@gmail.com>
2024-11-10 15:41:28 +0800

    init

---
+++ app.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+import uvicorn
+import os
+
+app = FastAPI()
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
+
+# Serve the favicon.ico
+@app.get("/favicon.ico", include_in_schema=False)
+async def favicon():
+    return FileResponse(os.path.join("static", "favicon.ico"))
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
